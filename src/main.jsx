9995e6c14da0b8f52a5faacbda4ab239a2ae56ca
auradavid <aura.prisacariu@gmail.com>
2022-11-08 08:29:49 +0200

    Add router and error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import ErrorPage from "./error-page";
 import App from './App';
 import './index.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
