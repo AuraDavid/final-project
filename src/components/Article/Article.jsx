9f62193c89ea516de805447d41b64c8c76e10e5b
auradavid <aura.prisacariu@gmail.com>
2022-11-13 21:12:59 +0200

    Added responsiveness to main page and section component

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,14 +1,11 @@
 import { useEffect, useState } from "react";
 import styles from "./Article.module.css"
 import { useLocation } from 'react-router-dom'
-import { useLoaderData } from "react-router-dom";
 
 const Article = (props) => {
     const location = useLocation();
     const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
     const [article, setArticle] = useState([]);
-
-    const test = useLoaderData();
 
     useEffect(() => {
         fetch(`${location.state.apiUrl}?api-key=${API_KEY}&show-fields=headline,body`)
